739c1207f4bd5dd8465baeb8b8daf981d28d69ce
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-02 17:23:53 -0500

    Initial file tree and some link content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>header</li>
+          <li>particle</li>
+          <li>image</li>
+          <Navbar />
+          <li>footer</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "./Link";
+
+const Navbar = props => {
+  return (
+    <>
+      <Link>Apps</Link>
+      <Link>Projects</Link>
+      <Link>CV</Link>
+      <Link>Blog</Link>
+    </>
+  );
+};
+
+export default Navbar;
