a4878aef94b61107a0402f1e81d6c173b7b84af9
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-04 17:26:52 -0500

    Add node-sass

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import classes from "./header.module.css";
+import classes from "./header.module.scss";
 
 const Header = props => {
   return (
     <div className={classes.Header}>
       <div className={classes.title}>
-        <header>Edgar Cuarezma</header>
+        <header>
+          <h1>Edgar Cuarezma</h1>
+          <h3>Web App Developer</h3>
+        </header>
       </div>
       <div className={classes.polygon}></div>
     </div>
