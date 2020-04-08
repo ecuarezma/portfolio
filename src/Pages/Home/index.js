a1a266a90f8948dd2e4f087740ea9467906dbd4f
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-08 17:23:03 -0500

    Add chevron icon, fix layout so all content visible on scroll

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,10 +5,32 @@ import profile from "../../assets/profile.jpg";
 const Home = props => {
   return (
     <div className={classes.Home}>
-      <div className={classes.polygon}>
-        <img className={classes.profile} src={profile} alt="profile" />
+      <div className={classes.profile}>
+        <img className={classes.profileImage} src={profile} alt="profile" />
       </div>
+
+      <div className={classes.chevronDown}>
+        <div></div>
+        <div></div>
+      </div>
+
       <article className={classes.content}>
+        <p>
+          Having walked many paths in life, I strive to learn the depths of web
+          programming.
+        </p>
+        <p>
+          Having walked many paths in life, I strive to learn the depths of web
+          programming.
+        </p>
+        <p>
+          Having walked many paths in life, I strive to learn the depths of web
+          programming.
+        </p>
+        <p>
+          Having walked many paths in life, I strive to learn the depths of web
+          programming.
+        </p>
         <p>
           Having walked many paths in life, I strive to learn the depths of web
           programming.
