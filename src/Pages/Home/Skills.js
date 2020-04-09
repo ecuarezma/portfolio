7afbfd629b1d4d61631b8d28fcd4f1635c64fe06
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-08 21:23:50 -0500

    Style skill icons

--- src/Pages/Home/Skills.js
+++ src/Pages/Home/Skills.js
@@ -1,13 +1,17 @@
 import React from "react";
+import classes from "./skills.module.scss";
 
 const Skills = () => {
   return (
-    <div>
-      <div>
+    <div className={classes.Skills}>
+      <div className={classes.icons}>
         <i class="fab fa-js-square"></i>
-        <i class="fab fa-html5"></i>
         <i class="fab fa-css3-alt"></i>
+        <i class="fab fa-html5"></i>
         <i class="fab fa-react"></i>
+      </div>
+      <div className={classes.icons}>
+        <i class="fab fa-node-js"></i>
       </div>
     </div>
   );
