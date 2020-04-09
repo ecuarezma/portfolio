68ca5f711fd34e3822b62d76c0eeea1988f569c1
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-08 22:17:29 -0500

    Create colors sass module to globally import color variables

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./content.module.css";
+import classes from "./content.module.scss";
 
 const Content = props => {
   return <div className={classes.Content}>{props.children}</div>;
