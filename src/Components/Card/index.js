a54cedc5a2f10376c681ab845732092edb706362
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-13 13:52:32 -0500

    Add styling to Card component

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,14 +3,11 @@ import classes from "./Card.module.scss";
 
 const Card = props => {
   return (
-    <>
-      <div className={classes.Card}>
-        <h4>{props.title}</h4>
-        <img src={props.src} alt="" />
-        <figcaption>{props.children}</figcaption>
-      </div>
-      <div className={classes.background} />
-    </>
+    <div className={classes.Card}>
+      <h4>{props.title}</h4>
+      <img src={props.src} alt="" />
+      <figcaption>{props.children}</figcaption>
+    </div>
   );
 };
 
