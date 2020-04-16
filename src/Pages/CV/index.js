7d3211731dc8a066cbb7e8fe24ff5c4dacf4d486
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-15 22:02:57 -0500

    Add content to projects and cv page

--- src/Pages/CV/index.js
+++ src/Pages/CV/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import resume from "../../assets/dev_resume.pdf";
 
-const CV = props => {
-  return <div>CV page!</div>;
+const CV = (props) => {
+  return (
+    <div>
+      <img src="resume" alt="" />
+    </div>
+  );
 };
 
 export default CV;
--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Card from "../../Components/Card";
+import colorApp from "../../assets/colorApp.png";
+import entrante from "../../assets/entrante.png";
 
-const Projects = props => {
-  return <div>Projects page!</div>;
+import classes from "./Projects.module.scss";
+
+const Projects = () => {
+  return (
+    <article className={classes.Projects}>
+      <Card src={colorApp} title="Color App">
+        Create a custom palette!
+      </Card>
+      <Card src={entrante} title="Entrante">
+        Online showcase of artist's work.
+      </Card>
+    </article>
+  );
 };
 
 export default Projects;
