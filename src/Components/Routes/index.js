4d99d79fdf0b06173f1fd98c0994fc90ead342ca
Edgar Cuarezma <ecuarezma@gmail.com>
2020-04-21 15:30:08 -0500

    Add React transition group and apply CSSTransition to routes

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,19 +1,74 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+
 import Home from "../../Pages/Home";
 import Apps from "../../Pages/Apps";
 import Projects from "../../Pages/Projects";
 import CV from "../../Pages/CV";
+import "./styles.css";
 
 const Routes = () => {
+  const routes = [
+    { path: "/", name: "Home", Component: Home },
+    { path: "/apps", name: "Apps", Component: Apps },
+    { path: "/projects", name: "Projects", Component: Projects },
+  ];
+
   return (
-    <Switch>
-      <Route path="/" exact render={() => <Home />} />
+    <>
+      {/* <Route path="/" exact render={() => <Home />} />
       <Route path="/Apps" exact render={() => <Apps />} />
       <Route path="/Projects" exact render={() => <Projects />} />
-      <Route path="/CV" exact render={() => <CV />} />
-    </Switch>
+      <Route path="/CV" exact render={() => <CV />} /> */}
+      {routes.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+          {({ match }) => (
+            <CSSTransition
+              in={match != null}
+              timeout={300}
+              classNames="page"
+              unmountOnExit
+            >
+              <div className="page">
+                <Component />
+              </div>
+            </CSSTransition>
+          )}
+        </Route>
+      ))}
+    </>
   );
 };
 
 export default Routes;
+
+// function Example() {
+//   return (
+//     <Router>
+//       <>
+//         <Container className="container">
+//           {routes.map(({ path, Component }) => (
+//             <Route key={path} exact path={path}>
+//               {({ match }) => (
+//                 <CSSTransition
+//                   in={match != null}
+//                   timeout={300}
+//                   classNames="page"
+//                   unmountOnExit
+//                 >
+//                   <div className="page">
+//                     <Component />
+//                   </div>
+//                 </CSSTransition>
+//               )}
+//             </Route>
+//           ))}
+//         </Container>
+//       </>
+//     </Router>
+//   )
+// }
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<Example />, rootElement)
