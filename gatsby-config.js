4da24bbc612ec8ce6de1736e54274b0dcb186547
Edgar Cuarezma <ecuarezma@gmail.com>
2022-11-07 16:49:23 -0600

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  plugins: [`gatsby-plugin-fontawesome-css`],
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
   // plugins: [
   //   {
   //     resolve: 'gatsby-plugin-manifest',
