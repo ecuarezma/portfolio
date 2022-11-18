ce536de1478b4326f08654095eabfe2fa0fc6eca
Edgar Cuarezma <ecuarezma@gmail.com>
2022-11-18 14:49:42 -0600

    Fix css transition for card-info component

---
+++ src/components/Card/CardInfo/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import classes from "./CardInfo.module.scss";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  box-sizing: border-box;
+  position: relative;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.borderBlue};
+  padding: 1rem;
+  text-align: center;
+  z-index: -100;
+  @media (min-width: 600px) {
+    margin: 0 10%;
+  }
+  @media (min-width: 900px) {
+    margin: 0 15%;
+  }
+  @media (min-width: 1200px) {
+    margin: 0 20%;
+  }
+  figcaption {
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.mainBgColor};
+  }
+`;
+
+const CardInfo = ({ children }) => (
+  <StyledWrapper>
+    <div className="info">
+      <figcaption id="info">{children}</figcaption>
+    </div>
+  </StyledWrapper>
+);
+
+export default CardInfo;
---
+++ src/components/Header/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import ParticlesComponent from "../Particles";
+import { Link } from "gatsby";
+import "@fontsource/raleway";
+import styled from "styled-components";
+
+const StyledWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  z-index: 200;
+
+  .title {
+    margin-top: 1rem;
+    font-family: "Raleway", sans-serif;
+    color: rgb(253, 245, 230);
+    z-index: 1000;
+    text-decoration: none;
+    cursor: pointer;
+    span {
+      display: none;
+    }
+    @media (min-width: 600px) {
+      h1,
+      h3,
+      span {
+        display: inline;
+      }
+      margin: 1rem 0;
+      h1 {
+        letter-spacing: 0.2rem;
+        font-size: 2.5rem;
+      }
+      h3 {
+        letter-spacing: 0.2rem;
+        font-size: 1.5rem;
+      }
+      span {
+        margin: 0.5rem;
+        font-size: 3rem;
+        opacity: 0.5;
+      }
+    }
+    @media (max-height: 600px) and (min-width: 600px) {
+      margin: 0;
+      h1 {
+        letter-spacing: 0.2rem;
+        font-size: 2rem;
+      }
+      h3 {
+        letter-spacing: 0.2rem;
+        font-size: 1.2rem;
+      }
+    }
+    h1,
+    h3 {
+      text-align: center;
+      margin: 0;
+      padding: 0;
+    }
+    h3 {
+      font-weight: 200;
+    }
+  }
+`;
+
+const Header = () => (
+  <>
+    <StyledWrapper>
+      <Link to="/" className="title">
+        <h1>Edgar Cuarezma</h1>
+        <span>|</span>
+        <h3>Full Stack Developer</h3>
+      </Link>
+    </StyledWrapper>
+    <ParticlesComponent />
+  </>
+);
+
+export default Header;
